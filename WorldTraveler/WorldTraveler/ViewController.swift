6335a65386d8d3e5fe042455bcd256a511dcf737
ingaklassy <iklassy@ucsd.edu>
2018-09-19 00:07:01 -0700

    Finished initial UI

---
+++ WorldTraveler/WorldTraveler/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  WorldTraveler
+//
+//  Created by Inga Klassy on 9/18/18.
+//  Copyright © 2018 Inga Klassy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+}
+
